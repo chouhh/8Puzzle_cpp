56772b53467a71970d619ba976ef9a05cec27ca0
sjean <sjean@sjean-MacBook>
2016-01-22 01:11:04 +0800

    new one

---
+++ 8Puzzle/src/8Puzzle.cpp
@@ -0,0 +1,18 @@
+//============================================================================
+// Name        : 8Puzzle.cpp
+// Author      : sjean
+// Version     : 2016.1.15
+// Copyright   : Your copyright notice
+// Description : C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+
+	return 0;
+}
