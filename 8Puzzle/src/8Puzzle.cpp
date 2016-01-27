c2dc42a590d2bb6374d3e7aa9fdf31165efc411c
chouhh <chouhh@mail.cjcu.edu.tw>
2016-01-27 09:22:53 +0800

    finish subroutine

--- 8Puzzle/src/8Puzzle.cpp
+++ 8Puzzle/src/8Puzzle.cpp
@@ -1,7 +1,7 @@
 //============================================================================
 // Name        : 8Puzzle.cpp
 // Author      : sjean
-// Version     : 2016.1.15
+// Version     : 2016.1.27
 // Copyright   : Your copyright notice
 // Description : C++, Ansi-style
 //============================================================================
@@ -11,9 +11,11 @@
 
 using namespace std;
 
+long long int data[362880]; // 9! size
+int num_data;
 int dir[4][2] = {{-1,0},{0,1},{1,0},{0,-1}}; // up, right, down, left
 
-long long int encode(int b[3][3], int d)
+long long int encode(int b[3][3], int d) // encode the board
 {
 	long long result = 0;
 
@@ -30,7 +32,30 @@ long long int encode(int b[3][3], int d)
 	return result;
 }
 
-int** clone(int b[3][3])
+int** get_board(long long int d)
+{
+	int result[3][3];
+
+	d = d >> 4;
+
+	for (int i=2; i>=0; i--)
+	{
+		for (int j=2; j>=0; j--)
+		{
+			result[i][j] = d % 16;
+			d = d >> 4;
+		}
+	}
+
+	return result;
+}
+
+int get_depth(long long int d) // get depth of the board from data
+{
+	return d % 16;
+}
+
+int** clone(int b[3][3]) // clone the board
 {
 	int result[3][3];
 
@@ -45,27 +70,27 @@ int** clone(int b[3][3])
 	return result;
 }
 
-int** move(int b[3][3], int x, int y, int d)
+int** move(int b[3][3], int x, int y, int d) // make move and return the result board
 {
 	int result[3][3];
-	int newX = x + dir[d][0];
-	int newY = y + dir[d][1];
+	int nextX = x + dir[d][0];
+	int nextY = y + dir[d][1];
 
 	result = clone(b);
 
 	int temp = result[x][y];
-	result[x][y] = result[newX][newY];
-	result[newX][newY] = temp;
+	result[x][y] = result[nextX][nextY];
+	result[nextX][nextY] = temp;
 
 	return result;
 }
 
-bool can_move(int b[3][3], int x, int y, int d)
+bool is_legal_move(int x, int y, int d) // check whether the move on the direction is legal  or not
 {
-	int newX = x + dir[d][0];
-	int newY = y + dir[d][1];
+	int nextX = x + dir[d][0];
+	int nextY = y + dir[d][1];
 
-	if (newX >=0 && newX <=2 && newY >=0 && newY <=2)
+	if (nextX >=0 && nextX <=2 && nextY >=0 && nextY <=2)
 	{
 		return true;
 	}
@@ -77,21 +102,25 @@ bool can_move(int b[3][3], int x, int y, int d)
 
 int main()
 {
-	long long int data[362880]; // 9! size
-	int num_data = 0;
 	int board[3][3] = {{1,2,3},{8,0,4},{7,6,5}}; // initial the goal board
 	int depth = 0;
+	int visit_pos = 0;
 	bool can_move = true;
 
 	memset(data,-1,sizeof(data));
 	data[0] = encode(board,depth);
-	num_data++;
+	num_data = 1;
 
 	while (can_move)
 	{
 		can_move = false;
+		board = get_board(data[visit_pos]);
+		depth = get_depth(data[visit_pos]);
 
+		for (int i=0; i<4; i++)
+		{
 
+		}
 	}
 
 	return 0;
