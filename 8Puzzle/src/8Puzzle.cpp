ef5a574770cd032307950c893ca59d0887bd6300
chouhh <chouhh@mail.cjcu.edu.tw>
2016-01-27 20:01:14 +0800

    done but need test

--- 8Puzzle/src/8Puzzle.cpp
+++ 8Puzzle/src/8Puzzle.cpp
@@ -23,7 +23,7 @@ long long int encode(int b[3][3], int d) // encode the board
 	{
 		for (int j=0; j<3; j++)
 		{
-			result = result << 4 + b[j][i];
+			result = (result << 4) + b[j][i];
 		}
 	}
 
@@ -32,10 +32,8 @@ long long int encode(int b[3][3], int d) // encode the board
 	return result;
 }
 
-int** get_board(long long int d)
+void get_board(long long int d, int result[3][3])
 {
-	int result[3][3];
-
 	d = d >> 4;
 
 	for (int i=2; i>=0; i--)
@@ -46,8 +44,6 @@ int** get_board(long long int d)
 			d = d >> 4;
 		}
 	}
-
-	return result;
 }
 
 int get_depth(long long int d) // get depth of the board from data
@@ -55,10 +51,8 @@ int get_depth(long long int d) // get depth of the board from data
 	return d % 16;
 }
 
-int** clone(int b[3][3]) // clone the board
+void clone(int b[3][3], int result[3][3]) // clone the board
 {
-	int result[3][3];
-
 	for (int i=0; i<3; i++)
 	{
 		for (int j=0; j<3; j++)
@@ -66,23 +60,18 @@ int** clone(int b[3][3]) // clone the board
 			result[i][j] = b[i][j];
 		}
 	}
-
-	return result;
 }
 
-int** move(int b[3][3], int x, int y, int d) // make move and return the result board
+void make_move(int b[3][3], int x, int y, int d, int result[3][3]) // make move and return the result board
 {
-	int result[3][3];
 	int nextX = x + dir[d][0];
 	int nextY = y + dir[d][1];
 
-	result = clone(b);
+	clone(b, result);
 
-	int temp = result[x][y];
-	result[x][y] = result[nextX][nextY];
-	result[nextX][nextY] = temp;
-
-	return result;
+	int temp = result[y][x];
+	result[y][x] = result[nextY][nextX];
+	result[nextY][nextX] = temp;
 }
 
 bool is_legal_move(int x, int y, int d) // check whether the move on the direction is legal  or not
@@ -102,11 +91,11 @@ bool is_legal_move(int x, int y, int d) // check whether the move on the directi
 
 bool has_existed(int b[3][3])
 {
-	long long int code = encode(b,0) / 16;
+	long long int code = encode(b,0) >> 4;
 
 	for (int i=0; i<num_data; i++)
 	{
-		long long int existed_code = data[i] / 16;
+		long long int existed_code = data[i] >> 4;
 
 		if (existed_code == code)
 		{
@@ -117,28 +106,71 @@ bool has_existed(int b[3][3])
 	return false;
 }
 
+int get_blank_x(int b[3][3])
+{
+	for (int i=0; i<3; i++)
+	{
+		for (int j=0; j<3; j++)
+		{
+			if (b[i][j] == 0)
+			{
+				return j;
+			}
+		}
+	}
+
+	return -1;
+}
+
+int get_blank_y(int b[3][3])
+{
+	for (int i=0; i<3; i++)
+	{
+		for (int j=0; j<3; j++)
+		{
+			if (b[i][j] == 0)
+			{
+				return i;
+			}
+		}
+	}
+
+	return -1;
+}
+
 int main()
 {
 	int board[3][3] = {{1,2,3},{8,0,4},{7,6,5}}; // initial the goal board
 	int depth = 0;
 	int visit_pos = 0;
-	bool can_move = true;
 
 	memset(data,-1,sizeof(data));
 	data[0] = encode(board,depth);
 	num_data = 1;
 
-	while (can_move)
+	while (visit_pos < num_data) // has further move
 	{
-		can_move = false;
-		board = get_board(data[visit_pos]);
+		get_board(data[visit_pos], board);
 		depth = get_depth(data[visit_pos]);
 
-		for (int i=0; i<4; i++)
+		for (int i=0; i<4; i++) // consider 4 direction moves
 		{
+			int x = get_blank_x(board);
+			int y = get_blank_y(board);
 
+			if (is_legal_move(x,y,i) && !has_existed(board))
+			{
+				int new_board[3][3];
+				make_move(board,x,y,i,new_board);
+				data[num_data] = encode(new_board,depth+1);
+				num_data++;
+			}
 		}
+
+		visit_pos++; // visit next board
 	}
+
+	cout << "num_data = " << num_data;
 
 	return 0;
 }
